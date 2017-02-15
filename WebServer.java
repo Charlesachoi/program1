16885141950caa46ca60a5bdca398180faf6e4f7
Charles Choi <charlesachoi@gmail.com>
2017-02-14 19:06:03 -0700

    Project 2(P2): Updated WebWorker.java to be able to stream images to server. Added favicon support.

--- WebServer.java
+++ WebServer.java
@@ -1,97 +1,263 @@
 /**
-* A simple web server: it creates a new WebWorker for each new client
-* connection, so all the WebServer object does is listen on the port
-* for incoming client connection requests.
+* Web worker: an object of this class executes in its own new thread
+* to receive and respond to a single HTTP request. After the constructor
+* the object executes on its "run" method, and leaves when it is done.
 *
-* This class contains the application "main()" (see below). At startup, 
-* main() creates an object of this class (WebServer) and invokes its
-* start() method. Since servers run continually, the start() method 
-* never returns. It uses socket programming to listen for client network
-* connection requests. When one happens, it creates a new object of
-* the WebWorker class and hands that client connection off to the WebWorker
-* object. The WebServer object then just keeps listening for new client
-* connections. See the WebWorker source for more information about it.
-* 
+* One WebWorker object is only responsible for one client connection.
+* This code uses Java threads to parallelize the handling of clients:
+* each WebWorker runs in its own thread. This means that you can essentially
+* just think about what is happening on one client at a time, ignoring
+* the fact that the entirety of the webserver execution might be handling
+* other clients, too.
+*
+* This WebWorker class (i.e., an object of this class) is where all the
+* client interaction is done. The "run()" method is the beginning -- think
+* of it as the "main()" for a client interaction. It does three things in
+* a row, invoking three methods in this class: it reads the incoming HTTP
+* request; it writes out an HTTP header to begin its response, and then it
+* writes out some HTML content for the response content. HTTP requests and
+* responses are just lines of text (in a very particular format).
+*
 **/
 
-import java.net.*;
+import java.net.Socket;
+import java.lang.Runnable;
+import java.io.*;
+import java.util.*;
+//import java.util.Date;
+import java.text.DateFormat;
+//import java.util.TimeZone;
+//import java.io.File;
+import java.awt.image.BufferedImage;
+//import java.io.ByteArrayInputStream;
+//import java.io.ByteArrayOutputStream;
+//import java.io.IOException;
+//import java.io.InputStream;
+import javax.imageio.ImageIO;
+import javax.xml.bind.DatatypeConverter;
+//import net.sf.image4j.*;
 
-public class WebServer
+public class WebWorker implements Runnable
 {
 
-private ServerSocket socket;
-private boolean running;
+private Socket socket;
 
 /**
-* Constructor
+* Constructor: must have a valid open socket
 **/
-private WebServer()
+public WebWorker(Socket s)
 {
-   running = false;
+   socket = s;
 }
 
 /**
-* Web server starting point. This method does not return until
-* the server is finished, so perhaps it should be named "runServer"
-* or something like that.
-* @param port is the TCP port number to accept connections on
+* Worker thread starting point. Each worker handles just one HTTP
+* request and then returns, which destroys the thread. This method
+* assumes that whoever created the worker created it with a valid
+* open socket object.
 **/
-private boolean start(int port)
+public void run()
 {
-   Socket workerSocket;
-   WebWorker worker;
+   String filePath = "";
+   System.err.println("Handling connection...");
    try {
-      socket = new ServerSocket(port);
+      InputStream  is = socket.getInputStream();
+      OutputStream os = socket.getOutputStream();
+      //readHTTPRequest(is);
+      filePath = readHTTPRequest(is);
+      System.out.println("FilePath = " + filePath);
+      String MimeType = "text/html";
+      File f = new File(filePath);
+      String extension = "";
+      if(f.exists() && !f.isDirectory()) {
+        extension = getFileExtension(f);
+        System.out.println("extension: "+extension);
+        if(extension.equals("png")) MimeType = "image/png";
+        else if (extension.equals("jpg")) MimeType = "image/jpg";
+        else if (extension.equals("gif")) MimeType = "image/gif";
+        else if (extension.equals("ico")) MimeType = "image/x-icon";
+      }
+
+      writeHTTPHeader(os,MimeType, filePath);
+      if((extension.equals("png"))||(extension.equals("jpg"))||(extension.equals("gif"))||(extension.equals("ico")))
+      {
+        writeImageContent(os, filePath, extension);
+      }
+      else{
+        writeStringContent(os, filePath);
+      }
+      os.flush();
+      socket.close();
    } catch (Exception e) {
-      System.err.println("Error binding to port "+port+": "+e);
-      return false;
+      System.err.println("Output error: "+e);
    }
+   System.err.println("Done handling connection.");
+   return;
+}
+
+/**
+* Read the HTTP request header.
+**/
+private String readHTTPRequest(InputStream is)
+{
+   String line;
+   String filePath = "";
+   BufferedReader r = new BufferedReader(new InputStreamReader(is));
    while (true) {
       try {
-         // wait and listen for new client connection
-         workerSocket = socket.accept();
+         while (!r.ready()) Thread.sleep(1);
+         line = r.readLine();
+         System.err.println("Request line: ("+line+")");
+
+         // If its the GET line of the HTTP header than grab just the url path of the file they are requesting.
+         if( line.startsWith("GET "))
+         {
+           System.err.println("GET LINE: ("+line+")");
+           String GetLineString = line.replace("GET /","");
+           String[] Getline = GetLineString.split(" ");
+           System.err.println("Getline[0]: " + Getline[0]);
+           // This assumes that there are no spaces in the name of the file. Additional processing would be necessary to handle spaces within the name.
+           filePath = Getline[0];
+         }
+
+         if (line.length()==0) break;
       } catch (Exception e) {
-         System.err.println("No longer accepting: "+e);
+         System.err.println("Request error: "+e);
          break;
       }
-      // have new client connection, so fire off a worker on it
-      worker = new WebWorker(workerSocket);
-      new Thread(worker).start();
    }
-   return true;
-} // end start
-
-/**
-* Does not do anything, since start() never returns.
-**/
-private boolean stop()
-{
-   return true;
+   return filePath;
 }
 
 /**
-* Application main: process command line and start web server; default
-* port number is 8080 if not given on command line.
+* Write the HTTP header lines to the client network connection.
+* @param os is the OutputStream object to write to
+* @param contentType is the string MIME content type (e.g. "text/html")
 **/
-public static void main(String args[])
+private void writeHTTPHeader(OutputStream os, String contentType, String filePath) throws Exception
 {
-   int port = 8080;
-   if (args.length > 1) {
-      System.err.println("Usage: java Webserver <portNumber>");
+    System.out.println("ContentType: "+contentType);
+    File f = new File(filePath);
+    if(f.exists() && !f.isDirectory()) {
+        Date d = new Date();
+        DateFormat df = DateFormat.getDateTimeInstance();
+        df.setTimeZone(TimeZone.getTimeZone("MST"));
+        os.write("HTTP/1.1 200 OK\n".getBytes());
+        os.write("Date: ".getBytes());
+        os.write((df.format(d)).getBytes());
+        os.write("\n".getBytes());
+        os.write("Server: Jon's very own server\n".getBytes());
+        //os.write("Last-Modified: Wed, 08 Jan 2003 23:11:55 GMT\n".getBytes());
+        //os.write("Content-Length: 438\n".getBytes());
+        os.write("Connection: close\n".getBytes());
+        os.write("Content-Type: ".getBytes());
+        os.write(contentType.getBytes());
+        os.write("\n\n".getBytes()); // HTTP header ends with 2 newlines
+        return;
+      //}
+    }
+    else{
+      Date d = new Date();
+      DateFormat df = DateFormat.getDateTimeInstance();
+      df.setTimeZone(TimeZone.getTimeZone("MST"));
+      os.write("HTTP/1.1 404 Not Found\n".getBytes());
+      os.write("Date: ".getBytes());
+      os.write((df.format(d)).getBytes());
+      os.write("\n".getBytes());
+      os.write("Server: Jon's very own server\n".getBytes());
+      //os.write("Last-Modified: Wed, 08 Jan 2003 23:11:55 GMT\n".getBytes());
+      //os.write("Content-Length: 438\n".getBytes());
+      os.write("Connection: close\n".getBytes());
+      os.write("Content-Type: ".getBytes());
+      os.write(contentType.getBytes());
+      os.write("\n\n".getBytes()); // HTTP header ends with 2 newlines
+      //    To send hardcoded Data
+         os.write("<html><head><title>404 - File or directory not found.</title></head><body>\n".getBytes());
+         os.write("<h3>404 Not Found</h3>\n".getBytes());
+         os.write("</body></html>\n".getBytes());
       return;
-   } else if (args.length == 1) {
+    }
+}
+
+private static String getFileExtension(File file) {
+    String fileName = file.getName();
+    if(fileName.lastIndexOf(".") != -1 && fileName.lastIndexOf(".") != 0)
+    return fileName.substring(fileName.lastIndexOf(".")+1);
+    else return "";
+}
+
+/**
+* Write the data content to the client network connection. This MUST
+* be done after the HTTP header has been written out.
+* @param os is the OutputStream object to write to
+**/
+private void writeStringContent(OutputStream os, String filePath) throws Exception
+{
+//  Reads the requested filePath and sends it back to the browser.
+    try {
+      BufferedReader br = new BufferedReader(new FileReader(filePath));
+
       try {
-         port = Integer.parseInt(args[0]);
-      } catch (Exception e) {
-         System.err.println("Argument must be an int ("+e+")");
-         return;
+          StringBuilder sb = new StringBuilder();
+          String line = br.readLine();
+
+          while (line != null) {
+              sb.append(line);
+              sb.append(System.lineSeparator());
+              line = br.readLine();
+          }
+          String fileContent = sb.toString();
+          Date d = new Date();
+          DateFormat df = DateFormat.getDateTimeInstance();
+          df.setTimeZone(TimeZone.getTimeZone("MST"));
+          fileContent = fileContent.replace("<cs371date>",df.format(d)); //Replace fileC
+          String server = "JFS Server";
+          fileContent = fileContent.replace("<cs371server>",server);
+          os.write(fileContent.getBytes());
+      } catch(IOException e) {
+        System.err.println("ERROR Reading file contents: "+e);
+        }
+        finally {
+          br.close();
       }
-   }
-   WebServer server = new WebServer();
-   if (!server.start(port)) {
-      System.err.println("Execution failed!");
-   }
-} // end main
+    } catch(IOException e) {
+      System.err.println("ERROR Reading File: "+e);
+    }
+}
+
+private void writeImageContent(OutputStream os, String filePath, String extension) throws Exception{
+
+  System.out.println("ICO: write image content, filePath: "+filePath);
+  byte[] imageInByte;
+  System.out.println("ICO: trying to read FileInputStream image: "+filePath);
+
+  //InputStream is;
+
+  String fromFileName = filePath;
+  //String toFileName = "favicon.ico";
+
+  BufferedInputStream in = new BufferedInputStream(new FileInputStream(fromFileName));
+  //BufferedOutputStream out = new BufferedOutputStream(new FileOutputStream(toFileName));
+
+  byte[] buff = new byte[32 * 1024];
+  int len = 0;
+  while((len = in.read(buff)) > 0)
+  {
+    //out.write(buff, 0, len);
+    os.write(buff);
+  }
+  in.close();
+        //List<BufferedImage> image = ICODecoder.read(new File("input.ico"));
+
+        // convert byte array back to BufferedImage
+        // InputStream in = new ByteArrayInputStream(imageInByte);
+        // BufferedImage bImageFromConvert = ImageIO.read(in);
+        //ImageIO.write(bImageFromConvert, "png", new File("images/tinyking.png"));
+
+  //    To send hardcoded Data
+  //    os.write("<html><head></head><body>\n".getBytes());
+  //    os.write("<h3>Jon Waz Here!!!!!</h3>\n".getBytes());
+  //    os.write("</body></html>\n".getBytes());
+}
 
 } // end class
-
--- WebWorker.java
+++ WebWorker.java
@@ -3,12 +3,12 @@
 * to receive and respond to a single HTTP request. After the constructor
 * the object executes on its "run" method, and leaves when it is done.
 *
-* One WebWorker object is only responsible for one client connection. 
+* One WebWorker object is only responsible for one client connection.
 * This code uses Java threads to parallelize the handling of clients:
 * each WebWorker runs in its own thread. This means that you can essentially
-* just think about what is happening on one client at a time, ignoring 
+* just think about what is happening on one client at a time, ignoring
 * the fact that the entirety of the webserver execution might be handling
-* other clients, too. 
+* other clients, too.
 *
 * This WebWorker class (i.e., an object of this class) is where all the
 * client interaction is done. The "run()" method is the beginning -- think
@@ -16,25 +16,21 @@
 * a row, invoking three methods in this class: it reads the incoming HTTP
 * request; it writes out an HTTP header to begin its response, and then it
 * writes out some HTML content for the response content. HTTP requests and
-* responses are just lines of text (in a very particular format). 
+* responses are just lines of text (in a very particular format).
 *
 **/
 
 import java.net.Socket;
 import java.lang.Runnable;
 import java.io.*;
-import java.util.Date;
+import java.util.*;
 import java.text.DateFormat;
-import java.util.TimeZone;
 
 public class WebWorker implements Runnable
 {
 
 private Socket socket;
-private String pathName;
-private Boolean locExists;
-private Boolean locDirectory;
-private File checkFile;
+private boolean isRoot;
 
 /**
 * Constructor: must have a valid open socket
@@ -45,20 +41,47 @@ public WebWorker(Socket s)
 }
 
 /**
-* Worker thread starting point. Each worker handles just one HTTP 
+* Worker thread starting point. Each worker handles just one HTTP
 * request and then returns, which destroys the thread. This method
 * assumes that whoever created the worker created it with a valid
 * open socket object.
 **/
 public void run()
 {
+   String filePath = "";   
    System.err.println("Handling connection...");
    try {
       InputStream  is = socket.getInputStream();
       OutputStream os = socket.getOutputStream();
-      readHTTPRequest(is);
-      writeHTTPHeader(os,"text/html"); //change header type of request
-      writeContent(os);                
+      filePath = readHTTPRequest(is);
+      String fileType = "text/html";
+      File fileCheck = new File(filePath);
+      String extension = "";
+      //if it has image extension change file type to proper image type
+      //otherwise read it as a non image file.
+      if(fileCheck.exists() && !fileCheck.isDirectory()) 
+      {
+        extension = getFileExtension(fileCheck);
+        if(extension.equals("png")) 
+        	fileType = "image/png";
+        else if (extension.equals("jpg")) 
+        	fileType = "image/jpg";
+        else if (extension.equals("gif")) 
+        	fileType = "image/gif";
+        else if (extension.equals("ico")) 
+        	fileType = "image/x-icon";
+      }//if    
+       	  
+      writeHTTPHeader(os,fileType, filePath);
+      //if extension is an image/icon type run writeImageContent
+      //else run writeStringContent
+      if((extension.equals("png"))||(extension.equals("jpg"))||(extension.equals("gif"))||(extension.equals("ico")))
+      {
+        writeImageContent(os, filePath);
+      }
+      else{
+        writeStringContent(os, filePath);
+      }
       os.flush();
       socket.close();
    } catch (Exception e) {
@@ -71,61 +94,42 @@ public void run()
 /**
 * Read the HTTP request header.
 **/
-private void readHTTPRequest(InputStream is)
+private String readHTTPRequest(InputStream is)
 {
    String line;
-   int count = 0;
-   
+   String filePath = "";
    BufferedReader r = new BufferedReader(new InputStreamReader(is));
-   
    while (true) {
-	   
-	   count++;
-	   
       try {
-    	  
          while (!r.ready()) Thread.sleep(1);
          line = r.readLine();
-          
-         //execute once
-         if (count == 1)
-         {
-                 
-           String strAr[] = line.split(" "); 
-           checkFile = new File(strAr[1].substring(1)); 
-           
-           //If Root Directory
-           if(strAr[1].equals("/")) 
-           {
-              locExists = true; 
-              locDirectory = true;
-           }//root
-           
-           //If File Found
-           else if(checkFile.exists())
-           {
-              locExists = true;
-              locDirectory = false;
-           }//found
-           
-           //If File Not Found
-           else
-           {
-              locExists = false;
-              locDirectory = false; 
-           }//not found
-           
-           pathName = line.substring(5, line.indexOf(' ', 4)); //global variable with path
-         }
-         
          System.err.println("Request line: ("+line+")");
+
+         String strAr[] = line.split(" ");
+         //If Root Directory set isRoot to true
+         if(strAr[1].equals("/")) 
+         {
+            isRoot = true; 
+         }//root
+         
+         // If its the GET line of the HTTP header than grab just the url path of the file they are requesting.
+         if( line.startsWith("GET "))
+         {
+           System.err.println("GET LINE: ("+line+")");
+           String GetLineString = line.replace("GET /","");
+           String[] Getline = GetLineString.split(" ");
+           System.err.println("Getline[0]: " + Getline[0]);
+           // This assumes that there are no spaces in the name of the file. Additional processing would be necessary to handle spaces within the name.
+           filePath = Getline[0];
+         }
+
          if (line.length()==0) break;
       } catch (Exception e) {
          System.err.println("Request error: "+e);
          break;
       }
    }
-   return;
+   return filePath;
 }
 
 /**
@@ -133,28 +137,61 @@ private void readHTTPRequest(InputStream is)
 * @param os is the OutputStream object to write to
 * @param contentType is the string MIME content type (e.g. "text/html")
 **/
-private void writeHTTPHeader(OutputStream os, String contentType) throws Exception
+private void writeHTTPHeader(OutputStream os, String contentType, String filePath) throws Exception
 {
-   Date d = new Date();
-   DateFormat df = DateFormat.getDateTimeInstance();
-   df.setTimeZone(TimeZone.getTimeZone("GMT"));
-   
-   if (locExists == true)
-      os.write("HTTP/1.1 200 OK\n".getBytes());
-   else 
-      os.write("HTTP/1.1 404 File Not Found\n".getBytes());
-   
-   os.write("Date: ".getBytes());
-   os.write((df.format(d)).getBytes());
-   os.write("\n".getBytes());
-   os.write("Server: Charles' very own server\n".getBytes());
-   //os.write("Last-Modified: Wed, 08 Jan 2003 23:11:55 GMT\n".getBytes());
-   //os.write("Content-Length: 438\n".getBytes()); 
-   os.write("Connection: close\n".getBytes());
-   os.write("Content-Type: ".getBytes());
-   os.write(contentType.getBytes());
-   os.write("\n\n".getBytes()); // HTTP header ends with 2 newlines
-   return;
+    System.out.println("ContentType: "+contentType);
+    File f = new File(filePath);
+    if(f.exists() && !f.isDirectory()) {
+        Date d = new Date();
+        DateFormat df = DateFormat.getDateTimeInstance();
+        df.setTimeZone(TimeZone.getTimeZone("MST"));
+        os.write("HTTP/1.1 200 OK\n".getBytes());
+        os.write("Date: ".getBytes());
+        os.write((df.format(d)).getBytes());
+        os.write("\n".getBytes());
+        os.write("Server: Charles' very own server\n".getBytes());
+        os.write("Connection: close\n".getBytes());
+        os.write("Content-Type: ".getBytes());
+        os.write(contentType.getBytes());
+        os.write("\n\n".getBytes()); // HTTP header ends with 2 newlines
+        return;
+      //}
+    }
+    //if no file path is requested and only the local host and it's port is given Display message
+	else if (isRoot == true)
+	{
+			os.write("<html><head></head><body>\n".getBytes());
+			os.write("<center><h3>Charles Choi's Web Server</h3></center>\n".getBytes());
+			os.write("<center><h3>This is the root of the server.</h3></center>\n".getBytes());
+			os.write("</body></html>\n".getBytes());
+	}
+    else{
+      Date d = new Date();
+      DateFormat df = DateFormat.getDateTimeInstance();
+      df.setTimeZone(TimeZone.getTimeZone("MST"));
+      os.write("HTTP/1.1 404 Not Found\n".getBytes());
+      os.write("Date: ".getBytes());
+      os.write((df.format(d)).getBytes());
+      os.write("\n".getBytes());
+      os.write("Server: Charles' very own server\n".getBytes());
+      os.write("Connection: close\n".getBytes());
+      os.write("Content-Type: ".getBytes());
+      os.write(contentType.getBytes());
+      os.write("\n\n".getBytes()); // HTTP header ends with 2 newlines
+      //    To send hardcoded Data
+         os.write("<html><head><title>404 File Not Found.</title></head><body>\n".getBytes());
+         os.write("<h3>404 File Not Found</h3>\n".getBytes());
+         os.write("</body></html>\n".getBytes());
+      return;
+    }
+}
+
+//gets file extension from the path that has been inputed.
+private static String getFileExtension(File file) {
+    String fileName = file.getName();
+    if(fileName.lastIndexOf(".") != -1 && fileName.lastIndexOf(".") != 0)
+    return fileName.substring(fileName.lastIndexOf(".")+1);
+    else return "";
 }
 
 /**
@@ -162,45 +199,54 @@ private void writeHTTPHeader(OutputStream os, String contentType) throws Excepti
 * be done after the HTTP header has been written out.
 * @param os is the OutputStream object to write to
 **/
-private void writeContent(OutputStream os) throws Exception
+private void writeStringContent(OutputStream os, String filePath) throws Exception
 {
-		//if the html file exists in the given location
-		if ((locExists == true) && (locDirectory == false))
-		{     
-	         String fileContents;
-	         
-	         //Conversion of pathName to appropriate format
-	         FileInputStream fis = new FileInputStream(pathName);
-	         DataInputStream dis = new DataInputStream(fis);
-	         InputStreamReader isr = new InputStreamReader(dis);
-	         BufferedReader br = new BufferedReader(isr);
-	   
-	         while ((fileContents = br.readLine()) != null) 
-	         {
-	            //If html file has the tag <cs371date> write date
-	            if (fileContents.contains("<cs371date>"))
-	               os.write(new Date().toString().getBytes());
-	      
-	            //If html file has the tag <cs371server> write statement
-	            else if (fileContents.contains("<cs371server>"))
-	               os.write("You are at the Server (Port: 8080)".getBytes());
-	            //write html file contents
-	            else
-	               os.write(fileContents.getBytes());
-	   
-	         }  
-	   }
-	   
-	   //If there is no location given other than local host port.
-	   else if ((locExists == true) && (locDirectory == true))
-	   {
-			os.write("<html><head></head><body>\n".getBytes());
-			os.write("<center><h3>Charles Choi's Web Server</h3></center>\n".getBytes());
-			os.write("</body></html>\n".getBytes());
-	   }
-	   //If location was given but does not exist.
-	   else
-	      os.write("<center><h3>404 - File Not Found\n</h3></center>\n".getBytes());   
-}	
+//  Reads the requested filePath and sends it back to the browser.
+    try {
+      BufferedReader br = new BufferedReader(new FileReader(filePath));
+
+      try {
+          StringBuilder sb = new StringBuilder();
+          String line = br.readLine();
+
+          while (line != null) {
+              sb.append(line);
+              sb.append(System.lineSeparator());
+              line = br.readLine();
+          }
+          String fileContent = sb.toString();
+          Date d = new Date();
+          DateFormat df = DateFormat.getDateTimeInstance();
+          df.setTimeZone(TimeZone.getTimeZone("MST"));
+          fileContent = fileContent.replace("<cs371date>",df.format(d)); //Replace
+          String server = "Charles' Server for CS371";
+          fileContent = fileContent.replace("<cs371server>",server);
+          os.write(fileContent.getBytes());
+      } catch(IOException e) {
+        System.err.println("ERROR Reading file contents: "+e);
+        }
+        finally {
+          br.close();
+      }
+    } catch(IOException e) {
+      System.err.println("ERROR Reading File: "+e);
+    }
+}
+
+//convert image files into byte content and stream it to client network connection
+private void writeImageContent(OutputStream os, String filePath) throws Exception{
+
+  String fileName = filePath;
+  BufferedInputStream in = new BufferedInputStream(new FileInputStream(fileName));
+
+  byte[] buff = new byte[32 * 1024];
+  int length = 0;
+  while((length = in.read(buff)) > 0)
+  {
+    os.write(buff);
+  }
+  in.close();
+
+}
 
 } // end class
